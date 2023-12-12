3b8996ff2a8e237fc57ff733b1d7cc355cbec3aa
Seth Crist <sethcrist50@gmail.com>
2023-12-12 14:34:17 -0600

    took some notes

---
+++ src/TypesVariablesSyntaxNotes.java
@@ -0,0 +1,31 @@
+import java.sql.SQLOutput;
+
+public class TypesVariablesSyntaxNotes {
+    public static void main(String[] args) {
+        System.out.println("This is an example of a statement");
+        System.out.println("notice that it ends with a semicolon.");
+        System.out.println("When we creat statements: they MUST end with a semicolon");
+        System.out.println("or java gets mad at us... so add the semicolon");
+
+        //this is a single line comment
+
+        byte smallNum = 19;
+        System.out.println(smallNum);
+        smallNum = 127;
+        smallNum++;
+        System.out.println(smallNum); //this gives us -128 because byte can only store up to 127 so it rolls over
+
+        int numDefault;
+        numDefault = 0; // MUST set a value for a variable before  accessing it
+        System.out.println(numDefault);
+
+        char firstLetterOfSeth = 'S'; //stored in a single set of '', this includes numbers
+
+        String message = "I'm a string!";
+        String quote = "\"Better out than in, I always say.\" - Shrek, but also joe Stephens?!";
+        System.out.println(message);
+        System.out.println(quote);
+
+        final String STUDENT_OF_IMUGI = "Seth \"star Student\" Crist";
+    }
+}
