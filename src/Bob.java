c79b2460e4fa7c08549eab3bdb8498c0854f0353
Seth Crist <sethcrist50@gmail.com>
2023-12-14 16:57:27 -0600

    finished the bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("lets talk to my boy Bob, say something");
+        String input = scanner.next();
+        if (input.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (input.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (input.trim().equals(" ")) {
+            System.out.println("Fine, be that way");
+        } else {
+            System.out.println("Whatever");
+        }
+//        System.out.println("lets talk to my boy Bob");
+        input = scanner.next();
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -29,6 +29,18 @@
 
 public class StringExercise {
     public static void main(String[] args) {
+//      For each of the following output examples: create a String variable that contains the desired output and print it out to the console,
+//      you can do this with only one String variable and one print statement for each output example
+//          "We don't need no education"
+//          "We don't need no thought control"
 
+
+//          "Check "this" out!, "s inside of "s!"
+
+
+//          "In windows, the main drive is usually C:\"
+
+
+//          "I can do backslashes \, double backslashes \\, and the amazing triple backslash \\\!"
     }
 }
