0e23674cb29b770e0b7c6c7bbb56b2a1946571e1
Seth Crist <sethcrist50@gmail.com>
2023-12-15 09:45:37 -0600

    made a correction and followed along with david

--- src/Bob.java
+++ src/Bob.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 //        System.out.println("lets talk to my boy Bob, say something");
 //        String input = scanner.next();
 //        if (input.endsWith("?")) {
@@ -15,7 +15,26 @@ public class Bob {
 //            System.out.println("Whatever");
 //        }
 ////        System.out.println("lets talk to my boy Bob");
-//        input = scanner.next();
+//        input = scanner.next()
 
+//-------davids way---------
+
+        System.out.println("hows it going, wanna chat");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (input.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (input.trim().equals(" ")) {
+                System.out.println("Fine, be that way!");
+            } else if (input.equals("banana")) {
+                System.out.println("later bro");
+                break;
+            } else {
+                System.out.println("Whatever");
+            }
+
+        }
     }
 }
